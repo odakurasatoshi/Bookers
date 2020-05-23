f1013442d395733d1da6fb2feb9271763375411b
vagrant <vagrant@localhost.localdomain>
2020-05-23 10:49:42 +0000

    [Add]4-8編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,19 +3,37 @@ class BooksController < ApplicationController
   end
 
   def index
-  	@book = Book.new
-  end
-
-  def show
+    @books = Book.all
   end
 
   def new
+    @book = Book.new
   end
 
-  def edit
+  def show
+    @book = Book.find(params[:id])
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
